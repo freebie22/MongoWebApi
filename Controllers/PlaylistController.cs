2df88bba9eb7f1da6d7db4182cdf1c8d6d482c4d
freebie22 <bojkov755@gmail.com>
2023-11-10 18:10:30 +0200

    Add project files.

---
+++ Controllers/PlaylistController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoWebApi.Models;
+using MongoWebApi.Services;
+
+namespace MongoWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PlaylistController : ControllerBase
+    {
+        private readonly MongoDbService _mongoDBService;
+
+        public PlaylistController(MongoDbService mongoDBService)
+        {
+            _mongoDBService = mongoDBService;
+        }
+
+        [HttpGet]
+        public async Task<List<Playlist>> GetPlaylists()
+        {
+            return await _mongoDBService.GetAsync();
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> CreatePlaylist([FromBody] Playlist playlist)
+        {
+            await _mongoDBService.CreateAsync(playlist);
+            return CreatedAtRoute(nameof(GetPlaylists), new { id = playlist.Id }, playlist);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> AddToPlaylist(string id, [FromBody] string movieId)
+        {
+            await _mongoDBService.AddToPlaylistAsync(id, movieId);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> RemoveFromPlaylist(string id)
+        {
+            await _mongoDBService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ Services/MongoDbService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoWebApi.Models;
+using Newtonsoft.Json;
+
+namespace MongoWebApi.Services
+{
+    public class MongoDbService
+    {
+        private readonly IMongoCollection<Playlist> _playlistCollection;
+
+        public MongoDbService(IOptions<MongoDbSettings> mongoDBsettings)
+        {
+            MongoClient client = new MongoClient(mongoDBsettings.Value.ConnectionURI);
+            IMongoDatabase database = client.GetDatabase(mongoDBsettings.Value.DatabaseName);
+            _playlistCollection = database.GetCollection<Playlist>(mongoDBsettings.Value.CollectionName);
+        }
+
+        public async Task CreateAsync(Playlist playlist)
+        {
+            await _playlistCollection.InsertOneAsync(playlist);
+            return;
+        }
+
+        public async Task<List<Playlist>> GetAsync()
+        {
+           var playlist = await _playlistCollection.Find(new BsonDocument()).ToListAsync();
+           var jsonPlaylist = JsonConvert.SerializeObject(playlist, Formatting.Indented);
+           File.WriteAllText("Playlist.json", jsonPlaylist);
+           return playlist;
+
+        }
+
+        public async Task AddToPlaylistAsync(string id, string movieId)
+        {
+            FilterDefinition<Playlist> filter = Builders<Playlist>.Filter.Eq("Id", id);
+            UpdateDefinition<Playlist> update = Builders<Playlist>.Update.AddToSet<string>("items", movieId);
+            await _playlistCollection.UpdateOneAsync(filter, update);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            FilterDefinition<Playlist> filter = Builders<Playlist>.Filter.Eq("Id", id);
+            await _playlistCollection.DeleteOneAsync(filter);
+        }
+    }
+}
